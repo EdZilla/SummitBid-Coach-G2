0eef76d50d23838ec2768a0bd2c893bdda84a154
Ed Young <ejy@SummitBid.com>
2011-09-08 21:32:44 -0600

    Incomplete - task : coach g2 basic early development tasks changed db to use mysql

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,15 @@
 dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+//    pooled = true
+//    driverClassName = "org.h2.Driver"
+//    username = "sa"
+//    password = ""
+	
+	pooled = true
+	driverClassName = "com.mysql.jdbc.Driver"
+	  username = "ejy"
+	  password = "gr00vy"
+	  intialSize = 10
+	  maxActive = 100
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -14,7 +21,10 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:/opt/data/db/coachg2/devDb"
+            //url = "jdbc:h2:mem:/opt/data/db/coachg2/devDb"
+			//url = "jdbc:h2:mem:/opt/data/db/coachg2/devDb"
+			url = "jdbc:mysql://localhost:3306/coachg2?autoReconnect=true"
+			
         }
     }
     test {
@@ -27,7 +37,8 @@ environments {
         dataSource {
             //dbCreate = "update"
 			dbCreate = "create-drop"
-            url = "jdbc:h2:/opt/data/db/coachg2/prodDb"
+            //url = "jdbc:h2:/opt/data/db/coachg2/prodDb"
+			url = "jdbc:mysql://localhost:3306/coachg2?autoReconnect=true"
         }
     }
 }
