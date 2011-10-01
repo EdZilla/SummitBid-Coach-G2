a5f39f055e126476e0ec10fc8b62f892a86d4d01
Edward Young <ed_young@cable.comcast.com>
2011-10-01 16:48:12 -0600

    Commented out the food creation to debug deployment issues.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,13 +7,13 @@ def init = { servletContext ->
 	
 		def fixtureLoader
 		
-		def myFood
-			10.times {
-				//println it
-				 myFood = Food.foodFactory("bagel-${it}", "einsteins")
-				 println myFood
-			}
-			println "Bootstrapped foods: " + Food.list()
+//		def myFood
+//			10.times {
+//				//println it
+//				 myFood = Food.foodFactory("bagel-${it}", "einsteins")
+//				 println myFood
+//			}
+//			println "Bootstrapped foods: " + Food.list()
 			
 		def user = new ShiroUser(username: "ed", passwordHash: new Sha256Hash("gr00vy").toHex()) 
 		user.addToPermissions("*:*") 
