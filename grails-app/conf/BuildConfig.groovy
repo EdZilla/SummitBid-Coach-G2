d32eade9bf59bc0cf575f056ffcee372fac179c0
Ed Young <ejy@SummitBid.com>
2011-12-04 16:32:27 -0700

    added geb and spock dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,6 +13,9 @@ grails.project.dependency.resolution = {
     }
     log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
+	
+	def gebVersion = "0.6.1"
+	def seleniumVersion = "2.0rc3"
 
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
@@ -21,7 +24,7 @@ grails.project.dependency.resolution = {
         grailsCentral()
 
         // uncomment these to enable remote dependency resolution from public Maven repositories
-        //mavenCentral()
+        mavenCentral()
         //mavenLocal()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
@@ -32,7 +35,16 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         runtime 'mysql:mysql-connector-java:5.1.16'
-		test "org.codehaus.groovy.modules.http-builder:http-builder:0.5.1"
+		test "org.codehaus.groovy.modules.http-builder:http-builder:0.5.1"		
+		test "org.codehaus.geb:geb-spock:$gebVersion"
+		
+		test "org.codehaus.geb:geb-junit4:0.6.1"
+		test 'org.seleniumhq.selenium:selenium-firefox-driver:2.0rc3'
+		test 'org.seleniumhq.selenium:selenium-chrome-driver:2.0rc3'
+		test 'org.seleniumhq.selenium:selenium-htmlunit-driver:2.0rc3'
+		
+		
+		
 		
     }
 
@@ -42,5 +54,11 @@ grails.project.dependency.resolution = {
         compile ":resources:1.0.2"
 
         build ":tomcat:$grailsVersion"
+		
+		test ":spock:0.6-SNAPSHOT"
+		//test ":spock:0.5-groovy-1.8"
+		test ":geb:$gebVersion"
+		
+		
     }
 }
