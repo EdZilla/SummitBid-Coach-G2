1f114724ad6768cb2e6e78b3ed491fa4dd544240
Ed Young <ejy@SummitBid.com>
2011-12-13 18:25:41 -0700

    adding ExerciseCRUDSpec.groovy

---
+++ test/functional/com/summitbid/coach/func/ExerciseCRUDSpec.groovy
@@ -0,0 +1,41 @@
+import geb.spock.GebReportingSpec
+
+import spock.lang.*
+
+import pages.*
+
+@Stepwise
+class ExerciseCRUDSpec extends GebReportingSpec {
+
+def "there are no people"() {
+
+}
+
+def "add a person"() {
+
+}
+
+def "enter the details"() {
+
+}
+
+def "check the entered details"() {
+
+}
+
+def "edit the details"() {
+
+}
+
+def "check in listing"() {
+
+}
+
+def "show person"() {
+
+}
+
+def "delete user"() {
+
+}
+}
