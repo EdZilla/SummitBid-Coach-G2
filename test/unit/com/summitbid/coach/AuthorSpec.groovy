97a3a7bc5e306952ef158b3f04fe6ae8df3a5678
Ed Young <ejy@SummitBid.com>
2011-12-15 11:46:57 -0700

    AuthorSpec is again extending spock.lang.Specification. Still not sure it that is going to solve the spock errors.

--- test/unit/com/summitbid/coach/AuthorSpec.groovy
+++ test/unit/com/summitbid/coach/AuthorSpec.groovy
@@ -9,9 +9,34 @@ import grails.plugin.spock.*
  * NOTE: This originally extended grails.plugin.spock.UnitSpec, but it errored out
  * because the mockDomain defined in the testing mixin clashed with the spock plugin UnitSpec.
  * According to LD the way to best handle it is to extend spock.lang.Specification.
+ *
+ * CASE 1: extend spock.lang.Specification. and run with grails test-app :spock
+ * the following doesn't result in a test failure, but it's an error: 
+ *  Error 2011-12-14 17:18:00,290 [Thread-17] ERROR plugins.DefaultGrailsPlugin  - 
+ *  Error configuration scaffolding: Error creating bean with name 'scaffoldedActionMap': Singleton bean creation not allowed while the singletons of this factory are in destruction (Do not request a bean from a BeanFactory in a destroy method implementation!)
+ *  Message: Error creating bean with name 'scaffoldedActionMap': Singleton bean creation not allowed while the singletons of this factory are in destruction (Do not request a bean from a BeanFactory in a destroy method implementation!)
+ *  
+ *  CASE 2: extend spock.lang.Specification. and run with grails test-app :spock
+ *  Thi time it fails with: 
+ *  No signature of method: com.summitbid.coach.AuthorSpec.mockDomain() is applicable for argument types: (java.lang.Class) values: [class com.summitbid.coach.Author]
+ * 
+ * CASE 3: extend grails.plugin.spock.UnitSpec. and run with grails test-app :spock
+ * Tests pass on laptop, passes on build server.
+ * 
+ * CASE 4: extend grails.plugin.spock.UnitSpec. and run with grails test-app -unit 
+ * fails with 
+ * Error Compilation error compiling [unit] tests: startup failed:
+ * /Users/eyoung2297k/development/src/SummitBid-Coach-G2/test/unit/com/summitbid/coach/AuthorSpec.groovy: -1: The return type of java.lang.Object mockDomain(java.lang.Class, java.util.List) in com.summitbid.coach.AuthorSpec is incompatible with void mockDomain(java.lang.Class, java.util.List) in grails.plugin.spock.UnitSpec
+ * . At [-1:-1]  @ line -1, column -1.
+ * 1 error
+ * 
+ * CASE 5: extend spock.lang.Specification and run with grails test-app -unit 
+ * Tests pass
+ * 
+ * 
  */
-//class AuthorSpec extends spock.lang.Specification {
-class AuthorSpec extends grails.plugin.spock.UnitSpec {
+class AuthorSpec extends spock.lang.Specification {
+//class AuthorSpec extends grails.plugin.spock.UnitSpec {
 
     def "feature method"() {
 		setup: 
