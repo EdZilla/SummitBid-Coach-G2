9f140465e1d3ae8c2927a7d81313ec55aa35cd86
Ed Young <ejy@SummitBid.com>
2011-12-14 17:01:30 -0700

    //class AuthorSpec extends spock.lang.Specification { class AuthorSpec extends grails.plugin.spock.UnitSpec {

--- test/unit/com/summitbid/coach/AuthorSpec.groovy
+++ test/unit/com/summitbid/coach/AuthorSpec.groovy
@@ -10,7 +10,8 @@ import grails.plugin.spock.*
  * because the mockDomain defined in the testing mixin clashed with the spock plugin UnitSpec.
  * According to LD the way to best handle it is to extend spock.lang.Specification.
  */
-class AuthorSpec extends spock.lang.Specification {
+//class AuthorSpec extends spock.lang.Specification {
+class AuthorSpec extends grails.plugin.spock.UnitSpec {
 
     def "feature method"() {
 		setup: 
