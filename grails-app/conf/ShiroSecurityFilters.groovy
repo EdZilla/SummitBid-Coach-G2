98b9288b71b664bcf73662859752e30e64d49087
Ed Young <ejy@SummitBid.com>
2011-12-29 22:10:12 -0700

    disabling shiro

--- grails-app/conf/ShiroSecurityFilters.groovy
+++ grails-app/conf/ShiroSecurityFilters.groovy
@@ -10,7 +10,7 @@ class ShiroSecurityFilters {
                 if (!controllerName) return true
 
                 // Access control by convention.
-                accessControl()
+                //accessControl()
             }
         }
     }
