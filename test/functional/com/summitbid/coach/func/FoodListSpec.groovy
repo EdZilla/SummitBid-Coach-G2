683834396f3030a3912bf033393a1aa6af1dc127
Ed Young <ejy@SummitBid.com>
2012-01-27 14:49:58 -0700

    removed the hardcoded url in the FoodListSpec

--- test/functional/com/summitbid/coach/func/FoodListSpec.groovy
+++ test/functional/com/summitbid/coach/func/FoodListSpec.groovy
@@ -20,15 +20,17 @@ class FoodListSpec extends GebReportingSpec {
 	def "List all Foods"() {
 		//println "List all exercises"
 		when:
-		def browser = new Browser()
-		browser.setBaseUrl( "http://localhost:8080/coach/" )
+		//def browser = new Browser()
+		//browser.setBaseUrl( "http://localhost:8080/coach/" )
 		
 		println "when go to FoodListPage"
-		browser.to FoodListPage
+		//browser.to FoodListPage
+		to FoodListPage
 		
 		then: 
 		print "then "
 		println "at FoodListPage"
-		browser.at FoodListPage
+		//browser.at FoodListPage
+		at FoodListPage
 	}
 }
